203d462f4e2979d5049df6ad7df502884c585084
Savannah <savmaxwell@gmail.com>
2022-02-10 12:42:41 -0700

    wip

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
+
 const server = express()
 
-server.get('/', (req, res) => {
+const PORT = process.env.PORT || 9001
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/hello', (req, res) => {
     res.send(`
-    <h1>Web50 Deployment Project</h1>
+    <h1>Hello! API is working</h1>
     `)
 })
 
-const PORT = process.env.PORT || 9001;
+server.use('*', (res, req) => {
+    res.send(`<h1>Web50 Deployment Project</h1>`)
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+
+
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
